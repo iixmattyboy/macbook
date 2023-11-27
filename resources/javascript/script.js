dd54cf115d1630d2fc0c0a5f526b9ed50b94eb8d
Matt M <145183361+iixmattyboy@users.noreply.github.com>
2023-11-27 20:39:10 +1030

    update to all code to add burger menu, and second div in html

--- resources/javascript/script.js
+++ resources/javascript/script.js
@@ -1,8 +1,12 @@
 function toggleNav() {
-    var sideNav = document.querySelector('.sidenav');
+    var sideNav = document.getElementById('sideNav');
+    var burgerContainer = document.querySelector('.burger-container');
+
     if (sideNav.style.width === '250px') {
         sideNav.style.width = '0';
+        burgerContainer.classList.remove('open');
     } else {
         sideNav.style.width = '250px';
+        burgerContainer.classList.add('open');
     }
 }
