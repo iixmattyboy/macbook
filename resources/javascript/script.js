02af2fc8f4cff6bc2d1872abe9c1b8effecc2e54
iixmattyboy <145183361+iixmattyboy@users.noreply.github.com>
2023-11-28 09:18:27 +1030

    update all
    added additional "why" div to index along with corresponding CSS.
    updated Script.js to auto close side menu when anchor clicked and added smooth scroll.

--- resources/javascript/script.js
+++ resources/javascript/script.js
@@ -18,3 +18,24 @@ function closeNav() {
     sideNav.style.width = '0';
     burgerContainer.classList.remove('open');
 }
+
+// Add this event listener to close the navigation menu when an anchor link is clicked
+document.querySelectorAll('.sidenav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        // Close the navigation menu
+        closeNav();
+
+        // Scroll smoothly to the target section
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
